53ba400f1fea4367935500c05dc0c87b0fb56dce
FVazquezRisso <facundovazquez321@gmail.com>
2023-11-02 11:18:00 -0300

    Integrate Swagger and manually document root route for testing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('App Controller')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @ApiOperation({ summary: '"Hello World"' })
+  @ApiResponse({ status: 200, description: 'Returns "Hello World"' })
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Deep Pharma API')
+    .setDescription('Documentation for Deep Pharma API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
