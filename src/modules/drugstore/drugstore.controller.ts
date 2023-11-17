24905bbc195db3208f7e4b120be10c6701e0c6d3
Moriones20 <moriones06082001@gmail.com>
2023-11-17 12:44:54 -0500

    Specified the name of functions in drugstore controller

--- src/modules/drugstore/drugstore.controller.ts
+++ src/modules/drugstore/drugstore.controller.ts
@@ -4,7 +4,6 @@ import {
   Delete,
   Get,
   Param,
-  ParseBoolPipe,
   ParseIntPipe,
   Patch,
   Post,
@@ -20,37 +19,43 @@ export class DrugstoreController {
   constructor(private readonly drugstoreService: DrugstoreService) {}
 
   @Get()
-  async findAll(
+  async getAllDrugstores(
     @Query('includeDeleted') includeDeleted = false,
   ): Promise<Drugstore[]> {
     return this.drugstoreService.findAll(includeDeleted);
   }
 
   @Get(':id')
-  async findById(@Param('id', ParseIntPipe) id: number): Promise<Drugstore> {
+  async getDrugstoreById(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<Drugstore> {
     return this.drugstoreService.findById(id);
   }
 
   @Post()
-  async create(@Body() drugstoreData: CreateDrugstoreDto): Promise<Drugstore> {
+  async createDrugstore(
+    @Body() drugstoreData: CreateDrugstoreDto,
+  ): Promise<Drugstore> {
     return this.drugstoreService.create(drugstoreData);
   }
 
   @Put(':id')
-  async update(
+  async updateDrugstore(
     @Param('id', ParseIntPipe) id: number,
     @Body() drugstoreData: UpdateDrugstoreDto,
   ): Promise<Drugstore> {
     return this.drugstoreService.update(id, drugstoreData);
   }
 
-  @Patch('delete/:id')
-  async softDelete(@Param('id', ParseIntPipe) id: number): Promise<Drugstore> {
+  @Patch(':id')
+  async softDeleteDrugstore(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<Drugstore> {
     return this.drugstoreService.softDelete(id);
   }
 
-  @Delete(':id')
-  async hardDelete(@Param('id', ParseIntPipe) id: number) {
+  @Delete('hardDelete/:id')
+  async hardDeleteDrugstore(@Param('id', ParseIntPipe) id: number) {
     return this.drugstoreService.hardDelete(id);
   }
 }
